30ed609dc1db2ac2607be302a80729c05572fe8b
franco-barboza27 <franco.barboza@ucas-edu.net>
2026-01-12 23:46:11 -0700

    Finished financial calc

--- individual_projects/financial.py
+++ individual_projects/financial.py
@@ -1,6 +1,17 @@
 # FB Financial Calculator P.2
 
-# Checks if it's valid, stolen from a past project
+# Checks if it's valid, stolen from a past project\
+
+def roundfunc(question):
+    while True:
+        try:
+            var = round(float(input(question)), 2)
+            break
+        except:
+            print("You inputed a string of some kind (letters)")
+
+    return var
+
 def inputchecker(rangeofchoices):
     while True:
             choicevar = input(f"Which one would you like to choose?(1~{rangeofchoices})")
@@ -22,19 +33,23 @@ def inputchecker(rangeofchoices):
         # check if it's valid
     # Ask how much money they are inserting
     # Divide the total they need by the total they amount they are adding
-    # say that # is the amount of the time frames they need (that they chose, line 11)
+    # say that # is the amount of the time frames they need
     # optionally: convert time frame to every other time frame and present it in that form as well (e.g. Amount:100, inserting: 10, timeframe: monthly, "It will take ten months, or 40 weeks, or .83 years, or 80 biweeks, or ~300 days")
     # return to the menu
 
 def savingstime():
 
-    while True:
-        try:
-            savings = round(float(input("How much do you want to have saved by the end?")), 2)
-            break
-        except:
-            print("You inputed a string of some kind (letters)")
+    savingstotal = roundfunc("How much do you want to have saved by the end?")
+    savingamount = roundfunc("How much do you add each time you input money")
+    print("You add money every:\n1.day\n2.week\n3.month\n4.year")
+    timekeep = {1:"day", 2:"week", 3:"month", 4:"year"}
+    timeframe = inputchecker(4)
 
+    mintimes = round((savingstotal/savingamount), 2)
+
+    print(f"It will take you {mintimes} {timekeep[timeframe]}s to save to {savingstotal:.2}$")
+
+    menu()
 
 # Compound Interest Calculator
     # Ask how much interest they have
@@ -50,7 +65,19 @@ def savingstime():
     # return to menu func
 
 def compoundinterest():
-    pass
+            
+    interest = roundfunc("What is your interest currently (out of 100, ex: 10'%' interest = 10 not .10)?")
+    interest = interest/100
+    startingamount = roundfunc("How much are you starting with?")
+    timeamount = roundfunc("How long will you leave it alone?")
+    timeamount = int(timeamount)
+
+    for i in range(timeamount+1):
+        startingamount = startingamount + (startingamount * interest)
+    
+    print(f"Your new amount of money will be {startingamount:.2}$")
+
+    menu()
 
 # Budget Allocator
     # ask how much money they have
@@ -69,7 +96,44 @@ def compoundinterest():
     # call menu
 
 def budget():
-    pass
+    def percentageinator(total, amount):
+        decimal = amount/total
+        percent = decimal * 100
+        return percent
+    spent = 0
+    money = roundfunc("What is your total amount of spendable money?")
+    categoryamount = int(input("How many spending categories do you have?"))
+
+    categoriesdict = {}
+    numcount = 1
+    for numcount in range(1, categoryamount+1):
+        print(f"category {numcount}:")
+        category = input("What is the name of this category?")
+        categoriesdict.update({numcount:category})
+        numcount += 1
+    
+    count = 0
+    categoriespercents = {}
+    for item in categoriesdict:
+        count += 1
+        print(f"{count}. {categoriesdict[count]}:")
+        moneyspent = roundfunc("How much did you/will you spend in this category?")
+        spent += moneyspent
+        categoriespercents.update({count:percentageinator(money, moneyspent)})
+    
+    count = 0
+    for item in categoriespercents:
+        count += 1
+        print(f"{categoriesdict[count]}: {categoriespercents[count]}%")
+    
+    if spent <= money:
+        spentpercent = percentageinator(money, spent)
+        print(f"You have {spent}$ left, or {spentpercent}% of your budget left.")
+    else:
+        print("You have went over your budget.")
+
+    menu()
+        
 
 # Sale Price Calculator
     # ask for the cost of an item
@@ -80,7 +144,13 @@ def budget():
     # display new cost
 
 def salesprice():
-    pass
+    cost = roundfunc("What is the cost of the item?")
+    discount = roundfunc("What the discount? (out of 100, ex: 10'%' off discount = 10 not .10)")
+    discount = discount/100
+
+    newcost = -1 * ((cost*discount) - cost)
+    print(f"The new cost is {newcost:.2f}")
+    menu()
 
 # Tip Calculator 
     # ask for bill
@@ -92,7 +162,13 @@ def salesprice():
     # display new cost and change variable
 
 def tipcalc():
-    pass
+    bill = roundfunc("What is the order cost?")
+    tip = roundfunc("How much are you tipping? (out of 100, ex: 10'%' off discount = 10 not .10)")
+    tip = tip/100
+    updbill = bill + (tip * bill)
+
+    print(f"The new order cost is {updbill}")
+    menu()
 # Menu func
     # Present the list of options (2-6)
     # Ask them which one they want to do
@@ -121,3 +197,5 @@ def menu():
         tipcalc()
 
 print("Hello, this is a general financial calculator!")
+
+menu()
