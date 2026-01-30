c29364cb81b8e8f8029cdf9e2700726dd7613ae5
franco-barboza27 <franco.barboza@ucas-edu.net>
2026-01-30 10:35:24 -0700

    tested stff

--- notes/test.py
+++ notes/test.py
@@ -3,6 +3,21 @@ def funcname(paraparaprpa):
     paraparaprpa = paraparaprpa + 1
     return paraparaprpa
 
+
+levelnum = 10
+
+testuple = ("Level", levelnum)
+
+level, value = testuple
+
+print(f"{level} {value}")
+
+value += 5
+
+print(f"{level} {value}")
+
+
+
 print(funcname(5))
 
 print("String")
