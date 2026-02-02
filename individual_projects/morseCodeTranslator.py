f989eb96b28811df3bc22037b0c4999c31cc519a
franco-barboza27 <franco.barboza@ucas-edu.net>
2026-02-01 21:02:11 -0700

    Worked on morse translator psuedocode

---
+++ individual_projects/morseCodeTranslator.py
@@ -0,0 +1,29 @@
+# FB CP2 Morse Code Translator
+
+# Create a dictionary with a key for the MORSE tuple and a key for the letters tuple.
+
+# Words to Morse translator
+    # Tell them that the coder will not work with any special character aside from the space button
+
+    # for every character in the string
+        # Check if the character is in the letters tuple
+            # if it is, get its index
+            # add the same index of the thing on the morse tuple to an empty string
+            # if it isn't, add a space to the empty string
+
+    # print the string
+    # return to the main menu
+
+# Morse to Words
+    # Looping
+        # Ask for the morse code (with spaces in between each distinct morse character)
+    # for character in the string
+        # if the character is a space
+            # add the character's index to a list
+            # if the character index is 0
+                # add it 
+    # Check if the character is in the morse tuple
+        # if it is, get its index
+        # add the same index of the thing on the string tuple to an empty string
+        # if it isn't, add a space to the empty string
+
