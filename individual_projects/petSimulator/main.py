eabd89f31b005dbdbadf5d49cdd405191927a91e
franco.barboza27 <franco.barboza@ucas-edu.net>
2026-03-18 10:51:24 -0600

    Started the psuedocode for pet sim

---
+++ individual_projects/petSimulator/main.py
@@ -0,0 +1,42 @@
+# FB Pet Simulator
+
+# Time conversion : 1 hour IRL = 1 day IGL
+
+# MAIN main menu
+
+    # Ask them to create a new game or load a save
+
+    # if loading a save:
+        # check if there are any save files (out of 3) that exist
+    # otherwise:
+        # ask what save they want to start their game in
+
+# loading a save
+
+    # get the save file
+    # for every pet in the save
+        # for every item in the save
+            # save the item to a list
+        
+        # make the pet a class object with all of the information (from the list made previously)
+
+# CREATING a save
+
+    # ask what file they want to save to
+        # create a file with that name
+            # add a tutorial pet called "first pet"
+        
+        # make the first pet a class object
+
+# either way, go to the main GAME loop
+
+# Game Loop
+
+    # Display the menu
+        # 1. View pets
+        # 2. Select Pets
+        # 3. Breed pets
+        # 4. Play a contest
+        # 5. Breed pets
+
+    
