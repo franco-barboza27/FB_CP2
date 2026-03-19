330a55971af4d91ee5a2dbf16567161ad3a2861d
franco.barboza27 <franco.barboza@ucas-edu.net>
2026-03-19 10:50:58 -0600

    Made more psuedocode

--- individual_projects/petSimulator/main.py
+++ individual_projects/petSimulator/main.py
@@ -1,5 +1,8 @@
 # FB Pet Simulator
 
+# PETS SHOULD HAVE
+    # Name, species, age, EXP, rank, happiness(fraction), hunger(fraction), thirst(fraction), health(fraction), athletics, beauty, family(list)
+
 # Time conversion : 1 hour IRL = 1 day IGL
 
 # MAIN main menu
@@ -34,9 +37,26 @@
 
     # Display the menu
         # 1. View pets
-        # 2. Select Pets
-        # 3. Breed pets
-        # 4. Play a contest
-        # 5. Breed pets
+        # 2. Breed pets
+        # 3. Play a contest
+        # 4. Breed pets
+        # 5. Grocery store
+        # 6. pets store
 
+    # 1. view pets
+        # display every pet name, age and health
+        # select or menu
+        # selecting
+            # display selected pet's information (with turtle, and ASCII)
+            # ask if they want to
+                # 1. feed
+                # 2. play
+                # 3. train
+                # 4. groom
     
+    # 2. breeding pets
+        # ask what species they would like to breed (cat, dog, or rabbit)
+            # display all pets owned that have that as their species (if only one, say they cannot breed a new animal because they only have the one)
+            
+            # ask what two they would like to breed
+            # make a new pet of the pet class with a random sprite from one of the parents
