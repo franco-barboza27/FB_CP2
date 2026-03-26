ffef5f4a20aa1a9678c584046d484124461f62aa
franco.barboza27 <franco.barboza@ucas-edu.net>
2026-03-26 10:51:20 -0600

    Started psuedocode

---
+++ individual_projects/gradeBook/src/gradeloop.py
@@ -0,0 +1,8 @@
+# Grade view loop
+
+# display menu:
+    # 1. Add student
+    # 2. Add grade
+    # 3. View student record
+    # 4. View all students
+    # 5. Class summary
---
+++ individual_projects/gradeBook/src/main.py
@@ -0,0 +1,13 @@
+# FB Grade book
+
+# Display Welcoming
+
+# Ask if they would like to begin the program
+    # if yes:
+        # GOTO initialize
+            # from initialize, get all the current classes existing
+        # ask which class they want to go to
+            # GOTO initialize
+                # from initialize, get the students for the class they chose
+            # GOTO 
+    # if no: exit program
